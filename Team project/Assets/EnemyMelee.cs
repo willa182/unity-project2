ddafafc2ea9883eda8004617752073e13fe4a23a
Masashi6969 <dariusvv85@gmail.com>
2023-10-18 19:37:42 +0200

    d

--- Team project/Assets/EnemyMelee.cs
+++ Team project/Assets/EnemyMelee.cs
@@ -1,129 +1,118 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyMelee : MonoBehaviour
 {
-    public float walkingRange = 5f;
-    public float idleTime = 2f;
-    public float chaseRange = 10f;
+    public float walkSpeed = 2f;
+    public float runSpeed = 5f;
+    public float chaseRange = 30f;
     public float attackRange = 2f;
-    public float screamDuration = 2f; // Duration of the scream in seconds
+    public float screamRange = 35f;
 
-    public float walkSpeed = 3f;  // Adjust the walk speed as needed
-    public float runSpeed = 6f;   // Adjust the run speed as needed
-
-    private Transform player;
     private NavMeshAgent navMeshAgent;
     private Animator animator;
-
-    private bool isChasing = false;
+    private Transform playerTransform;
+    private bool isScreaming = false;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
-        // Start the initial behavior
-        StartWalking();
+        StartCoroutine(EnemyBehavior());
+    }
+
+    IEnumerator EnemyBehavior()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(IdleState());
+            yield return StartCoroutine(WalkState());
+        }
+    }
+
+    IEnumerator IdleState()
+    {
+        animator.SetBool("IsIdle", true);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        animator.SetBool("IsIdle", false);
+    }
+
+    IEnumerator WalkState()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * 10f;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, 10f, NavMesh.AllAreas);
+
+        navMeshAgent.SetDestination(hit.position);
+
+        animator.SetBool("IsWalking", true);
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        animator.SetBool("IsWalking", false);
     }
 
     void Update()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        float distanceToPlayer = Vector3.Distance(playerTransform.position, transform.position);
 
-        if (distanceToPlayer < attackRange)
+        if (distanceToPlayer < screamRange && distanceToPlayer >= chaseRange)
         {
-            // Player is in attack range
-            SetAnimation("MeleeAttack");
-            navMeshAgent.isStopped = true;
-        }
-        else if (distanceToPlayer < chaseRange)
-        {
-            // Player is in chase range
-            if (!isChasing)
-            {
-                isChasing = true;
-                SetAnimation("Scream");
-                Invoke("StartRunAfterScream", screamDuration);
-            }
-            else
-            {
-                navMeshAgent.isStopped = false;
-                navMeshAgent.speed = runSpeed;
-                navMeshAgent.SetDestination(player.position);
-            }
+            // Player is in scream range but not in chase range
+            animator.SetBool("Scream", true);
+            isScreaming = true;
+
+            // Face the player when screaming
+            FacePlayer();
         }
         else
         {
-            // Player is out of chase range
-            if (isChasing)
+            // Player is either in chase range or out of scream range
+            animator.SetBool("Scream", false);
+            isScreaming = false;
+
+            // If player is in chase range, set IsRunning animation and run towards the player
+            if (distanceToPlayer < chaseRange)
             {
-                isChasing = false;
-                StopNavMeshAgent();
-                StartWalking();
+                animator.SetBool("IsRunning", true);
+                navMeshAgent.speed = runSpeed;
+                navMeshAgent.SetDestination(playerTransform.position);
+            }
+            else
+            {
+                animator.SetBool("IsRunning", false);
+                navMeshAgent.speed = walkSpeed;
+            }
+        }
+
+        if (isScreaming || distanceToPlayer < chaseRange)
+        {
+            // Disable other movements during scream or when in chase range
+            navMeshAgent.isStopped = true;
+            animator.SetBool("Melee", false);
+        }
+        else
+        {
+            // Enable movements when not screaming and not in chase range
+            navMeshAgent.isStopped = false;
+
+            if (distanceToPlayer < attackRange)
+            {
+                animator.SetBool("Melee", true);
+            }
+            else
+            {
+                animator.SetBool("Melee", false);
             }
         }
     }
 
-    void StartRunAfterScream()
+    void FacePlayer()
     {
-        SetAnimation("IsRunning");
-        navMeshAgent.SetDestination(player.position); // Ensure the destination is set after the scream
-    }
-
-    void SetAnimation(string animationBool)
-    {
-        // Reset all animation bools
-        animator.SetBool("IsWalking", false);
-        animator.SetBool("IsIdle", false);
-        animator.SetBool("IsRunning", false);
-        animator.SetBool("Melee", false);
-        animator.SetBool("Scream", false);
-
-        // Set the specified animation bool
-        animator.SetBool(animationBool, true);
-    }
-
-    void StartWalking()
-    {
-        // Set a random destination within walking range
-        Vector3 randomDestination = RandomNavmeshLocation(walkingRange);
-        navMeshAgent.speed = walkSpeed;  // Set walk speed
-        navMeshAgent.SetDestination(randomDestination);
-
-        SetAnimation("IsWalking");
-
-        // Schedule the transition to idle after reaching the destination
-        Invoke("StartIdle", navMeshAgent.remainingDistance / navMeshAgent.speed);
-    }
-
-    void StartIdle()
-    {
-        SetAnimation("IsIdle");
-
-        // Schedule the transition to walking after idling for a random time
-        Invoke("StartWalking", Random.Range(idleTime, idleTime * 2));
-    }
-
-    void StopNavMeshAgent()
-    {
-        navMeshAgent.isStopped = true;
-        navMeshAgent.enabled = false;
-    }
-
-    void ActivateNavMeshAgent()
-    {
-        navMeshAgent.enabled = true;
-        navMeshAgent.isStopped = false;
-    }
-
-    Vector3 RandomNavmeshLocation(float radius)
-    {
-        Vector3 randomDirection = Random.insideUnitSphere * radius;
-        randomDirection += transform.position;
-        NavMeshHit navHit;
-        NavMesh.SamplePosition(randomDirection, out navHit, radius, -1);
-        return navHit.position;
+        Vector3 directionToPlayer = (playerTransform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(directionToPlayer.x, 0, directionToPlayer.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
